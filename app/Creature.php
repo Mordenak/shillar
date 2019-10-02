09da6fde3888123a8920bcb749783f1ab151adc2
Nick <nicksolwick@gmail.com>
2019-10-02 10:40:17 -0700

    Another creature save fix

--- app/Creature.php
+++ app/Creature.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Creature extends Model
 {
-	protected $fillable = ['name', 'img_src', 'is_hostile', 'is_blocking', 'health', 'armor', 'damage_low', 'damage_high', 'attacks_per_round', 'magic_resistance', 'scroll_resistance', 'alignments_id', 'alignment_strength', 'award_xp', 'xp_variation', 'award_gold', 'gold_variation'];
+	protected $fillable = ['name', 'img_src', 'is_hostile', 'is_blocking', 'health', 'armor', 'damage_low', 'damage_high', 'attacks_per_round', 'attack_text', 'magic_resistance', 'scroll_resistance', 'alignments_id', 'alignment_strength', 'award_xp', 'xp_variation', 'award_gold', 'gold_variation'];
 
 	public function spawn_rules()
 		{
