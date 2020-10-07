5d490ce89874ff41f3f7beeb4218fb07b0cf11d6
Mordenak <nicksolwick@gmail.com>
2020-10-06 22:32:21 -0700

    upgrade laravel, fix food model. #test

--- app/ItemFood.php
+++ app/ItemFood.php
@@ -6,6 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class ItemFood extends Model
 {
+	protected $table = 'item_foods';
+
 	protected $fillable = ['items_id', 'name', 'potency'];
 
 	public function item()
