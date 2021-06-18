1bdb4e7d50afa49f16fc0aaabe7c0d4d34c3c7c7
Mordenak <nicksolwick@gmail.com>
2021-06-17 20:34:12 -0700

    potential DO fix

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -10,4 +10,6 @@ use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+
+    Request::setTrustedProxies(array($request->server->get('REMOTE_ADDR')));
 }
