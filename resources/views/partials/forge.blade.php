524836f10520265c82d1962ae7506a3816334cb4
Mordenak <nicksolwick@gmail.com>
2022-08-01 09:50:34 -0700

    forge fixes

--- resources/views/partials/forge.blade.php
+++ resources/views/partials/forge.blade.php
@@ -12,45 +12,45 @@ This is the Town Forge>
 	Weapon:
 	<select name="forge_weapon">
 		<option>-- Select --</option>
-	@foreach ($character->inventory()->character_items() as $char_item)
+	@foreach ($character->inventory()->unequipped_items() as $char_item)
 		@if ($char_item->item()->item_types_id == 1)
-		<option value="{{$char_item->id}}">{{$char_item->item()->name}}</option>
+		<option value="{{$char_item->item()->id}}">{{$char_item->item()->name}}</option>
 		@endif
 	@endforeach
 	</select>
 	Armor:
 	<select name="forge_armor">
 		<option>-- Select --</option>
-	@foreach ($character->inventory()->character_items() as $char_item)
+	@foreach ($character->inventory()->unequipped_items() as $char_item)
 		@if ($char_item->item()->item_types_id == 2)
-		<option value="{{$char_item->id}}">{{$char_item->item()->name}}</option>
+		<option value="{{$char_item->item()->id}}">{{$char_item->item()->name}}</option>
 		@endif
 	@endforeach
 	</select>
 	Food:
 	<select name="forge_food">
 		<option>-- Select --</option>
-	@foreach ($character->inventory()->character_items() as $char_item)
+	@foreach ($character->inventory()->unequipped_items() as $char_item)
 		@if ($char_item->item()->item_types_id == 4)
-		<option value="{{$char_item->id}}">{{$char_item->item()->name}}</option>
+		<option value="{{$char_item->item()->id}}">{{$char_item->item()->name}}</option>
 		@endif
 	@endforeach
 	</select>
 	Jewel:
 	<select name="forge_jewel">
 		<option>-- Select --</option>
-	@foreach ($character->inventory()->character_items() as $char_item)
+	@foreach ($character->inventory()->unequipped_items() as $char_item)
 		@if ($char_item->item()->item_types_id == 5)
-		<option value="{{$char_item->id}}">{{$char_item->item()->name}}</option>
+		<option value="{{$char_item->item()->id}}">{{$char_item->item()->name}}</option>
 		@endif
 	@endforeach
 	</select>
 	Dust:
 	<select name="forge_dust">
 		<option>-- Select --</option>
-	@foreach ($character->inventory()->character_items() as $char_item)
+	@foreach ($character->inventory()->unequipped_items() as $char_item)
 		@if ($char_item->item()->item_types_id == 6)
-		<option value="{{$char_item->id}}">{{$char_item->item()->name}}</option>
+		<option value="{{$char_item->item()->id}}">{{$char_item->item()->name}}</option>
 		@endif
 	@endforeach
 	</select>
